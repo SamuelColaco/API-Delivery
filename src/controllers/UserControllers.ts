a1f15581b17cea6d16405a49c3463ed490da3940
Samuel <colaco.samuel8@gmail.com>
2024-11-01 17:03:44 -0300

    Definidno rotas put e delete e controllers gerais

--- src/controllers/UserControllers.ts
+++ src/controllers/UserControllers.ts
@@ -1,16 +1,49 @@
 
 import { Request, Response, NextFunction } from "express"
 import { AppError } from "../utils/AppError"
+import { prisma } from "../../prisma/prisma"
+import z from "zod"
 
 export class UserControllers{
 
     async index(req: Request, res: Response){
+        const Users = await prisma.user.findMany()
         
-        res.status(200).json()
+        res.status(200).json(Users.length > 0 ? Users:  "Nada encontrado" ) 
     }
 
     async create(req: Request, res: Response){
         
+        const { name, email, password, role } = req.body
+        
+        const bodySchema = z.object({
+            name: z.string(),
+            email: z.string().trim().min(6),
+            password: z.string().trim().min(6),
+            role: z.string()
+        })
+
+        bodySchema.parse(req.body)
+
+        await prisma.user.create({data: { name, email, password, role }}) 
+
         res.status(201).json()
+    }
+
+    async update(req: Request, res: Response){
+        const { id } = req.params
+        const { ...all } = req.body
+
+        await prisma.user.update({where: { id }, data: { ...all}})
+
+        res.status(200).json()
+    }
+
+    async delete(req: Request, res: Response){
+        const { id } = req.params
+
+        await prisma.user.delete({where: { id }})
+
+        res.status(200).json()
     }
 }
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -7,6 +7,8 @@ const userControllers = new UserControllers()
 
 userRoutes.get("/", userControllers.index)
 userRoutes.post("/user", userControllers.create)
+userRoutes.put("/user/:id", userControllers.update)
+userRoutes.delete("/user/:id", userControllers.delete)
 
 
 export { userRoutes }
